4d6c34df7bebe3187bcf4cbf94abd91948de7a3e
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-05 09:32:19 -0500

    buenas

---
+++ hola_mundo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cout<< "Ingresar n: ";
+    cin>> N;
+    while ( N-- > 0)
+    {
+        cout << "Hello world!\n";
+    }
+    return 0;
+}
