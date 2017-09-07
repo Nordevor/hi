9efb694690a7e366aa979a9990db2da3c0ea596b
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-07 10:41:10 -0500

    fact

---
+++ factorial/bisiestofac/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+bool bisiesto(int anho){
+    int res = anho % 4 && (anho % 100!=0 || anho % 400==0);
+    if (res==0)
+        return true;
+    else
+        return false;
+}
+
+int main(){
+    int n;
+    cout << "Ingresar año: ";
+    cin>>n;
+    if (bisiesto(n))
+        cout<<"año bisiesto";
+    else
+        cout<<"No lo es";
+}
